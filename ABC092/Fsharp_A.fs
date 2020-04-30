0922009535a27c4fbaf80d381b054690202d317a
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-04-30 17:54:30 +0900

    Fsharp

--- ABC092/Fsharp_A.fs
+++ ABC092/Fsharp_A.fs
@@ -2,7 +2,7 @@ let a = stdin.ReadLine() |> int
 let b = stdin.ReadLine() |> int
 let c = stdin.ReadLine() |> int
 let d = stdin.ReadLine() |> int
- 
+
 let train = if a >= b then b else a
 let bus = if c >= d then d else c
  
